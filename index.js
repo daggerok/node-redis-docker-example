78a6f45cc5a07ab1acddd98c3b71ae2eb4b3cc53
Maksim Kostromin <daggerok@gmail.com>
2018-10-30 19:47:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const key = 'counter';
 client.set(key, 0);
 
 const app = express();
-app.get('/health', (req, res) =>
+app.get('/health/**', (req, res) =>
   res.send(JSON.stringify({ status: 'UP' }))
 );
 app.get('/', (req, res) => {
   client.get(key, (err, reply) => {
     if (err) {
-      res.send(JSON.stringify({
+      return res.send(JSON.stringify({
         error: err,
       }));
-      return;
     }
     res.send(JSON.stringify({
       counter: reply,
